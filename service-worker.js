c424eea092160da6b0ad2772bad865cefcfb8afc
arnavvivek <arnav.vivek1203@gmail.com>
2025-08-25 23:57:22 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE = "myghatkopar-v1";
+const ASSETS = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        return resp;
+      }).catch(() => caches.match(e.request))
+    );
+  }
+});
